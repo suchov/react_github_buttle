2500fdc3c2173e0902c8cfcc1bec3033cd1d85cc
suchov <artem.suchov@gmail.com>
2018-10-15 18:10:05 +0300

    refactor api.js my

--- app/utils/api.js
+++ app/utils/api.js
@@ -4,40 +4,40 @@ var id = "b1fa3ffbaabe25c4325a";
 var sec = "1b64570df834104e1c57babaa08a6fceb9181566";
 var params = "?client_id=" + id + "&client_secret=" + sec;
 
-function getProfile (username) {
+const getProfile = (username) => {
   return axios.get('https://api.github.com/users/' + username + params)
-    .then(function (user) {
+    .then((user) => {
       return user.data;
     });
 }
 
-function getRepos (username) {
+const getRepos = (username) => {
   return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
 }
 
-function getStarCount (repos) {
-  return repos.data.reduce(function (count, repo) {
+const getStarCount = (repos) => {
+  return repos.data.reduce((count, repo) => {
     return count + repo.stargazers_count
   }, 0);
 }
 
-function calculateScore (profile, repos) {
+const calculateScore = (profile, repos) => {
   var followers = profile.followers;
   var totalStars = getStarCount(repos);
 
   return (followers * 3) + totalStars;
 }
 
-function handleError (error) {
+const handleError = (error) => {
   console.warn(error);
   return null;
 }
 
-function getUserData (player) {
+const getUserData = (player) => {
   return axios.all([
     getProfile(player),
     getRepos(player)
-  ]).then(function (data) {
+  ]).then((data) => {
     var profile = data[0];
     var repos = data[1];
 
@@ -48,23 +48,23 @@ function getUserData (player) {
   });
 }
 
-function sortPlayers (players) {
+const sortPlayers = (players) => {
   return players.sort(function (a,b) {
     return b.score - a.score;
   });
 }
 
 module.exports = {
-  battle: function (players) {
+  battle: (players) => {
     return axios.all(players.map(getUserData))
       .then(sortPlayers)
       .catch(handleError);
   },
-  fetchPopularRepos: function (language) {
+  fetchPopularRepos: (language) => {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
     return axios.get(encodedURI)
-      .then(function (response) {
+      .then((response) => {
         return response.data.items;
       });
   }
